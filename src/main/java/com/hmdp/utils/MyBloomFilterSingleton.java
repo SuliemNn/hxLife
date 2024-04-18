e09ff9d360fceeb08928897549d9cb0c728a35d5
SuliemNn <SuliemNn@outlook.com>
2024-04-18 16:30:36 +0800

    删除MyBloomFilter等三个无用文件

--- src/main/java/com/hmdp/utils/MyBloomFilterSingleton.java
+++
@@ -1,8 +0,0 @@
-package com.hmdp.utils;
-
-public class MyBloomFilterSingleton {
-    private static MyBloomFilter instance = new MyBloomFilter();
-    public static MyBloomFilter getInstance(){
-        return instance;
-    }
-}
